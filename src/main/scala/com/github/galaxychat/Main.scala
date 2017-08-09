b53b0292f4c8afb49880b7401ee6e8b3af4cc079
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-08-09 14:28:57 -0300

    Iniciando a implementação do Server.

--- src/main/scala/com/github/galaxychat/Main.scala
+++ src/main/scala/com/github/galaxychat/Main.scala
@@ -1,9 +1,27 @@
 package com.github.galaxychat
 
+import java.io.File
+
+import akka.actor._
+import com.typesafe.config.ConfigFactory
+
+class Server extends Actor {
+
+  def receive = {
+    case _ => // TODO
+  }
+
+}
+
 object Main {
 
   def main(args: Array[String]) {
-    println("Galaxy IRC!")
+
+    val configFile = getClass.getClassLoader.getResource("application.conf").getFile
+    val config = ConfigFactory.parseFile(new File(configFile))
+    val system = ActorSystem("GalaxyChatServerSystem", config)
+    val server = system.actorOf(Props[Server], name = "server")
+
   }
 
 }
