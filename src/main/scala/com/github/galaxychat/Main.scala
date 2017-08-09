df157ff83d8080916cf0ab609e877d8256cffbbc
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-08-09 16:13:31 -0300

    Implementando a mensagem para efetuar a conexão.

--- src/main/scala/com/github/galaxychat/Main.scala
+++ src/main/scala/com/github/galaxychat/Main.scala
@@ -5,10 +5,20 @@ import java.io.File
 import akka.actor._
 import com.typesafe.config.ConfigFactory
 
+case class User(val nick: String, val addr: ActorRef)
+case class Connection(val user: User)
+
 class Server extends Actor {
 
+  var users: List[User] = List()
+
   def receive = {
-    case _ => // TODO
+
+    case Connection(user) => {
+      users = users ++ List(user)
+      println("Connection accepted.")
+    }
+
   }
 
 }
@@ -21,6 +31,9 @@ object Main {
     val config = ConfigFactory.parseFile(new File(configFile))
     val system = ActorSystem("GalaxyChatServerSystem", config)
     val server = system.actorOf(Props[Server], name = "server")
+
+    println("\n\nGalaxy Chat Server")
+    println("Waiting for incoming connections...")
 
   }
 
