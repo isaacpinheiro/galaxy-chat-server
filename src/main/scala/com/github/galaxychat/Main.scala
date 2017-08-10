fec3f49555eb6e93164075c4e8ef414f478eec33
Isaac Pinheiro <isaacpnhr@gmail.com>
2017-08-10 12:45:44 -0300

    Implementando o envio de mensagens, respostas e listagem de usuários.

--- src/main/scala/com/github/galaxychat/Main.scala
+++ src/main/scala/com/github/galaxychat/Main.scala
@@ -7,6 +7,9 @@ import com.typesafe.config.ConfigFactory
 
 case class User(val nick: String, val addr: ActorRef)
 case class Connection(val user: User)
+case class Message(val nick: String, val content: String)
+case class Response(val content: String)
+case class ListUsers(val nick: String)
 
 class Server extends Actor {
 
@@ -17,6 +20,22 @@ class Server extends Actor {
     case Connection(user) => {
       users = users ++ List(user)
       println("Connection accepted.")
+    }
+
+    case Message(nick, content) => {
+      val msg = nick + ": " + content
+      users.foreach((u: User) => { u.addr ! Response("\n" + msg + "\n") })
+    }
+
+    case ListUsers(nick) => {
+
+      val msg = "\nUsers:\n" + users.foldLeft("")((res: String, u: User) => {
+        res + u.nick + "\n"
+      })
+
+      val addr = users.filter((u: User) => u.nick == nick).head.addr
+      addr ! Response(msg)
+
     }
 
   }
